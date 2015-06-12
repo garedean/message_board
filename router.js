3f79e2d9dc869003955e6e8a360b04273119d4f0
Garrett <garrettdolson@gmail.com>
2015-06-12 14:21:41 -0700

    Set up routes

--- router.js
+++ router.js
@@ -1,4 +1,7 @@
 App.Router.map(function() {
     this.resource('questions', {path: '/'});
+    this.resource('question', {path: ':post_id'}, function() {
+      this.resource('new-answer');
+    });
     this.resource('new-question');
 });
